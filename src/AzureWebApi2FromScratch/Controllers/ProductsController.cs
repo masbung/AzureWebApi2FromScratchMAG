585e3ab2694dd8b2297cfa0408210268ef691780
masbung <masbung@sistrategia.com>
2017-06-13 11:46:33 -0500

    factory explained

--- src/AzureWebApi2FromScratch/Controllers/ProductsController.cs
+++ src/AzureWebApi2FromScratch/Controllers/ProductsController.cs
@@ -11,26 +11,54 @@ namespace AzureWebApi2FromScratch.Controllers
 {
     public class ProductsController : ApiController
     {
+        IProductService service;
+
+        public ProductsController() {
+            service = ProductServiceFactory.CreateProductService();            
+        }
+
+        //public ProductsController(IProductService service) {            
+        //    this.service = service;
+        //}
+
         // GET api/products        
         [SwaggerOperation("GetAllProducts")]
         public IEnumerable<Product> Get() {
-            ApplicationDbContext dbContext = new ApplicationDbContext();
-            return dbContext.Products.ToArray();
+            //IProductService service = new DbProductService();            
+            return service.GetAll();
+        }
+
+        // GET api/products        
+        [SwaggerOperation("GetProduct")]
+        public Product Get(int productId) {
+            //IProductService service = new DbProductService();
+            //IProductService service = ProductServiceFactory.CreateProductService();
+            return service.Get(productId);            
         }
       
         // POST api/products
         [SwaggerOperation("AddProduct")]
         [SwaggerResponse(HttpStatusCode.Created)]
         public IHttpActionResult Post([FromBody]Product product) {
-            try {
-                ApplicationDbContext dbContext = new ApplicationDbContext();                
-                dbContext.Products.Add(product);
-                dbContext.SaveChanges();                
+            //try {
+            //    ApplicationDbContext dbContext = new ApplicationDbContext();                
+            //    dbContext.Products.Add(product);
+            //    dbContext.SaveChanges();                
+            //    return Created(new Uri(Request.RequestUri + product.ProductId.ToString()), product);
+            //} catch {
+            //    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.NoContent);
+            //    return ResponseMessage(response);
+            //}
+
+
+            //IProductService service = new DbProductService();            
+            if (service.AddProduct(product)) {
                 return Created(new Uri(Request.RequestUri + product.ProductId.ToString()), product);
-            } catch {
+            } else {
                 HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.NoContent);
                 return ResponseMessage(response);
             }
+
         }       
     }
 }
--- src/AzureWebApi2FromScratch/Models/Product.cs
+++ src/AzureWebApi2FromScratch/Models/Product.cs
@@ -12,4 +12,67 @@ namespace AzureWebApi2FromScratch.Models
         public string ProductCategory { get; set; }
         public int Quantity { get; set; }   
     }
+
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int productId);
+        bool AddProduct(Product product);
+    }
+
+    //Database
+    public class DbProductService : IProductService
+    {
+
+        public IEnumerable<Product> GetAll() {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+            return dbContext.Products.ToArray();
+        }
+
+        public Product Get(int productId) {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+            return dbContext.Products.Find(productId);
+        }
+
+        public bool AddProduct(Product product) {
+            try {
+                ApplicationDbContext dbContext = new ApplicationDbContext();
+                dbContext.Products.Add(product);
+                dbContext.SaveChanges();
+                return true;
+            } catch {                
+                return false;
+            }
+        }
+    }
+
+
+    public class MemoryProductService : IProductService
+    {
+        private static Dictionary<int, Product> products;
+
+        static MemoryProductService() {
+            MemoryProductService.products = new Dictionary<int, Product>();
+        }
+
+        public IEnumerable<Product> GetAll() {
+            return MemoryProductService.products.Values.ToArray();            
+        }
+
+        public Product Get(int productId) {
+            return MemoryProductService.products[productId];
+        }
+
+        public bool AddProduct(Product product) {
+            MemoryProductService.products.Add(product.ProductId, product);
+            return true;
+        }
+    }
+
+    public class ProductServiceFactory
+    {
+        public static IProductService CreateProductService() {
+            return new MemoryProductService();
+        }
+    }
 }
